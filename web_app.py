c3562d1ef9b96d06610ad0beef727e3ef0dd1413
Naresh <nareshkadambini@gmail.com>
2023-02-27 15:20:07 -0500

    File ADD

---
+++ web_app.py
@@ -0,0 +1,30 @@
+from datetime import  datetime
+
+from pywebio.input import *
+from pywebio.output import *
+
+def current_time():
+    current_time = datetime.now().hour
+    # print(current_time)
+    return current_time
+
+def greetings():
+    time = int(current_time())
+    # print(time)
+    if time >12:
+        greet = "Good Morning. W "
+    elif time > 18:
+        greet = "Good Afternoon"
+    else:
+        greet = "Good Evening"
+    return greet
+
+def expense_entry():
+    greeting = greetings()
+    continue_check_box = checkbox(greeting, options = [" I would like to continue"])
+
+
+if __name__ == '__main__':
+    print(greetings())
+
+# start = input("")
